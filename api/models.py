9622c6a4de3a6df610c24e16564d13cdb680896f
ssengup <ssengup@gmail>
2022-01-18 03:19:07 +0530

    added basic stuff like models urls etc

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
+from optparse import Option
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    GENDERS = (
+        ('m' , 'MALE'),
+        ('f' , 'FEMALE'),
+        ('d' , 'DISCLOSED')
+    )
+    name = models.CharField(max_length=250)
+    rollnumber = models.IntegerField(unique= True)
+    email = models.EmailField(max_length=100)
+    gender = models.CharField(choices = GENDERS , max_length=1)
+    percentage = models.FloatField()
+    Institute = models.ForeignKey('Institute' , on_delete= models.CASCADE)
+
+class Institute(models.Model):
+    TYPE = (
+        ('s' ,'SCHOOL'),
+        ('h' , 'HIGHSCHOOL'),
+        ('c' , 'COLLEGE')
+    )
+    name = models.CharField(max_length=250)
+    type_of_Inst = models.CharField(choices= TYPE)
