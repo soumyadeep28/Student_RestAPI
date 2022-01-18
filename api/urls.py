21dff8cd38307d72e430d0b13a938fcacee52cb6
ssengup <ssengup@gmail>
2022-01-18 10:16:58 +0530

    successfully submitted all the operations successfully and Tested

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from api import views
 
 urlpatterns = [ 
-    path('viewstudent/' , views.StudentListview.as_view())
+    path('viewstudent/' , views.StudentListview.as_view()),
+    path('detailstudent/<int:pk>' , views.StudentDetailview.as_view()),
+    path('viewcreatestudent/' , views.StudenlistCreate.as_view()),
+    path('updatestudent/<int:pk>' , views.StudentUpdateview.as_view()),
+    path('deletestudent/<int:pk>' , views.Deletestudent.as_view())
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework.generics import (ListAPIView ,)
+from rest_framework.generics import (ListAPIView , RetrieveAPIView ,ListCreateAPIView , RetrieveUpdateAPIView ,
+DestroyAPIView ,RetrieveUpdateDestroyAPIView )
+# RetrieveUpdateDestroyAPIView makes the code all the operation in one 
 
 from api import models , serializers
 
@@ -8,3 +10,29 @@ from api import models , serializers
 class StudentListview(ListAPIView):
     queryset = models.Student.objects.all()
     serializer_class =serializers.StudentSerializers
+
+#if post then update else view all 
+class StudenlistCreate(ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializers
+
+class StudentDetailview(RetrieveAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializers
+
+#this is for update or fetch the details if patch then update the column
+class StudentUpdateview(RetrieveUpdateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializers
+
+#to test this use delete operations 
+class Deletestudent(DestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializers
+
+
+
+
+
+
+
